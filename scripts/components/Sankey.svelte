e06fbc2feea078b6a8a38d25b82170ab054034be
David Bruant <davidbruant@protonmail.com>
2021-04-14 00:24:44 +0200

    Déplacement de composants dans un dossier dédié + échaffaudage composant Sankey

---
+++ scripts/components/Sankey.svelte
@@ -0,0 +1,30 @@
+
+
+<svg viewBox="0,0,954,600">
+    <g stroke="#000">
+        <rect x="1" y="5" height="54.67966178640337" width="15" fill="#1f77b4">
+            <title>Dotation de l'État &gt;-187.1</title>
+        </rect>
+    </g>
+    <g fill="none" stroke-opacity="0.5">
+        <g style="mix-blend-mode: multiply;">
+            <path
+                d="M16,32.33983089320168C125.625,32.33983089320168,125.625,55.62943528217351,235.25,55.62943528217351"
+                stroke="#aaa"
+                stroke-width="54.67966178640335"
+            />
+            <title >Dotation de l'État → Recettes de fonctionnement &gt;-187.1</title >
+        </g>
+    </g>
+    <g font-family="sans-serif" font-size="10">
+        <text x="22" y="32.339830893201686" dy="0.35em" text-anchor="start" >Dotation de l'État</text>
+    </g>
+</svg>
+
+<style lang="scss">
+
+</style>
+
+<script>
+    //export let name;
+</script>
