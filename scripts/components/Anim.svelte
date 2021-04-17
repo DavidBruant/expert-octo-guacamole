e4a2bc200f2e51d4b32ad55a2570aacf2ec5dbb5
David Bruant <davidbruant@protonmail.com>
2021-04-17 23:32:32 +0200

    Jouer des éléments en séquence

--- scripts/components/Anim.svelte
+++ scripts/components/Anim.svelte
@@ -1,17 +1,23 @@
-<button on:click={increment}>+</button><button on:click={decrement}>-</button>
+
+{#if playing}
+    <button on:click={pause}>Pause</button>
+{:else}
+    <button on:click={play}>Play</button>
+{/if}
+
+<button on:click={startOver}>Start over</button>
+
 
 <section>
-{#each carrés as carré (carré.i)}
-    {#if shown.has(carré.i)}
-        <div class="carré" transition:fade></div>   
-    {/if}
+{#each appearingElements as carré (carré.i)}
+    <div class="carré" transition:fade on:introend={carré.introend} style="background-color: {carré.color}"></div>   
 {/each}
 </section>
 
 <style lang="scss">
     button{
-        width: 2rem;
-        height: 2rem;
+        min-width: 3rem;
+        min-height: 3rem;
     }
 
     .carré{
@@ -19,29 +25,75 @@
         margin-right: 1rem;
         width: 4rem;
         height: 4rem;
-        background-color: coral;
+        border: 3px solid #808080;
+
+        border-radius: 1rem;
     }
 </style>
 
 <script>
     import { fade, slide } from 'svelte/transition';
 
-    let maxShown = 2
+    let playing = false;
 
-    function increment(){
-        maxShown++
+    const count = 9;
+
+    const carrés = Array(count).fill().map((_, i) => {
+        const greyShade = Math.round(i*255/(count-1)).toString(16);
+        console.log('greyShade', greyShade)
+        return {i, color: `#${greyShade}${greyShade}${greyShade}`}
+    });
+
+    const apparitionSequence = [...carrés]
+
+    let nextToDisplayIndex;
+    let appearingElements;
+
+    function play(){
+        playing = true;
+
+        let previousElementDonePromise = Promise.resolve()
+
+        for(const element of apparitionSequence.slice(nextToDisplayIndex)){
+            previousElementDonePromise = previousElementDonePromise.then(() => {
+                if(playing){
+                    let thisElementResolve;
+                    const p = new Promise((resolve, reject) => {
+                        thisElementResolve = resolve
+                    })
+
+                    appearingElements[nextToDisplayIndex] = {
+                        ...element, 
+                        introend(){
+                            thisElementResolve()
+                        }
+                    }
+
+                    nextToDisplayIndex++
+
+                    return p;
+                }
+                else{
+                    return;
+                }
+
+            })
+        }
+
+        previousElementDonePromise.then(() => {playing = false})
+
     }
 
-    function decrement(){
-        maxShown--
+    function pause(){
+        playing = false;
     }
 
-    const count = 10;
+    function startOver(){
+        pause()
+        nextToDisplayIndex = 0;
+        appearingElements = []
+    }
 
-    const carrés = Array(count).fill().map((_, i) => ({i}));
-
-    let shown;
-
-    $: shown = new Set(carrés.filter(c => c.i <= maxShown).map(c => c.i))
+    startOver()
 
 </script>
